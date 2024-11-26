3b007d22f706fe057cebb833d1be8aa9a693c941
Wijdan Almutairi <wijdan.s.almutairi@gmail.com>
2024-11-26 19:49:54 +0300

    feat: create and style Home View with static data
    - designed and implemented the Home View layout
    - added styling to enhance the appearance of the Home View
    - populated the view with static placeholder data

---
+++ ExpensesApp/HomeView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  ExpensesApp
+//
+//  Created by Wijdan Almutairi on 26/11/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                // part one
+                ZStack {
+                    Rectangle()
+                        .frame(width: 300, height: 200)
+                        .foregroundColor(.accentColor)
+                    .cornerRadius(12)
+                    
+                    Text("50 SAR")
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .fontWeight(.semibold)
+                }.padding()
+               
+                List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                    Text("Expense \(item + 1)" )
+                        .padding(7)
+                }
+                .listStyle(.plain)
+                .padding(.top)
+            }
+            .padding()
+            .navigationTitle("Expenses")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                Button {
+                    // Show Add View
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
