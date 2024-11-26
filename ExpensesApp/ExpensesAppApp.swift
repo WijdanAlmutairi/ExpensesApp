f1521252339488a33ab9527cb0c7ecda5efe6a31
Wijdan Almutairi <wijdan.s.almutairi@gmail.com>
2024-11-26 19:08:31 +0300

    Initial Commit

---
+++ ExpensesApp/ExpensesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesAppApp.swift
+//  ExpensesApp
+//
+//  Created by Wijdan Almutairi on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
